7c72493cb318dafad921ab104f776bb708936dea
Mike Spade <mikespadeny@gmail.com>
2022-05-30 11:12:08 -0600

    Update block-scope.js

--- js/block-scope.js
+++ js/block-scope.js
@@ -1,4 +1,4 @@
-// Declare the city variable  gloabally outside the scope of the function block but can still access it for concatenation. Remember inner brackets for callback!!
+// Declare the city variable  globally outside the scope of the function block but can still access it for concatenation. Remember inner brackets for callback!!
 const city = 'New York City';
 function logCitySkyline () {
   let skyscraper = 'Empire State Building'
@@ -26,3 +26,30 @@ logVisibleLightWaves();
 console.log(lightWaves)
 
 
+// In this example, we have polluted the scope of the variable by reassigning the global variable. Use different naming conventions or use 'let' in function to retain the global value
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  stars = 'Sirius'
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars)
+
+// Inside the if block console.log(lightWaves) logs the value Northern Lights to the console. Outside the if block, but still within the function, the same statement logs Moonlight to the console.
+
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights'
+    console.log(lightWaves)
+  }
+  
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
