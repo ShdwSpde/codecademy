9c41bab657280ab9fd7e028a07600dae0b262760
Mike Spade <mikespadeny@gmail.com>
2022-05-30 19:23:44 -0600

    Create objects.js

---
+++ js/objects.js
@@ -0,0 +1,105 @@
+// We can create objects by using curly brackets and then defining the properties and values within them
+let fasterShip = {
+  'Fuel Type': 'Turbo Fuel',
+  color: 'silver'
+};
+
+
+//  You can use dot notation to access the value of specific properties of an object
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+const crewCount = spaceship.numCrew;
+
+const planetArray = spaceship.flightPath; 
+
+
+// You HAVE to use bracket notation to access the value of keys with a space, special character, or number in them
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+
+let propName =  'Active Mission';
+
+// Write your code below
+const isActive = spaceship['Active Mission'];
+
+console.log(spaceship['Active Mission']);
+
+
+
+// after defining the object, we can still use dot notation and square notation to update our keys. we can even add to the initial set of keys and "delete" values
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold';
+
+spaceship.numEngines = 8;
+
+delete spaceship['Secret Mission'];
+
+
+
+//  Objects can have their own functions; comma-separated, key-value pairs called methods. they can be invoked with dot notation
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+const alienShip = {
+  retreat () {
+    console.log(retreatMessage);
+  },
+  takeOff () {
+    console.log('Spim... Borp... Glix... Blastoff!');
+  }
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+
+
+// We can create an array of objects as a key within an object- our passengers on the spaceship. You can then use dot and bracket notation to access key values and re-assign them. remember indexes?? 
+let spaceship = {
+  passengers: [{name: 'Space Dog'}], 
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+
+let firstPassenger = spaceship.passengers[0];
+
+console.log(firstPassenger);
