f66f299611b284090cde5739080947132ed29f70
Mike Spade <mikespadeny@gmail.com>
2022-06-06 07:56:45 -0600

    Create setters.js

---
+++ js/setters.js
@@ -0,0 +1,100 @@
+// As we used getters to retrieve the value of a specific object property, we can similarly use setters to reassign them
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === 'number' && num >= 0){
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0')
+    }   
+  } 
+};
+
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+
+// Once we have our model, we can create a factory to nearly instantly create similar and repetitive objects. Below we create a robot factory with an arrow function and then create a specific tinCan model, notice how the functions stored still persist with directly assigning them a value
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+const tinCan = robotFactory('P-500', true)
+
+tinCan.beep()
+
+// We can save ourselves keystrokes using shorthand
+function robotFactory(model, mobile){
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
+
+
+// We can further shorten our syntax with destructured assignment. In destructured assignment we create a variable with the name of an object’s key that is wrapped in curly braces { } and assign to it the object.
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log('Beep Boop');
+    },
+    fireLaser() {
+      console.log('Pew Pew');
+    },
+  }
+};
+
+const { functionality } = robot;
+
+functionality.beep();
+
+
+
+// In the previous exercises we’ve been creating instances of objects that have their own methods. But, we can also take advantage of built-in methods for Objects!
+
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot)
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+
+console.log(newRobot);
